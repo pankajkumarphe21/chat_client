bf6daa3b8ef70f5ded844311d56744cb8300a1fe
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-06-05 21:48:02 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import io from 'socket.io-client';
 import { useState } from "react"
 import Chat from './Chat';
 
-const socket = io.connect("http://localhost:3001")
+const socket = io.connect("https://chat-three-roan-94.vercel.app/")
 
 function App() {
   const [userName, setUserName] = useState("");
